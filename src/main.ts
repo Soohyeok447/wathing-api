13a802a4b9c5fa5448a561381ea8017c8e26ed32
Soohyeok Kim <devshk447@gmail.com>
2024-11-14 18:56:16 +0900

    CORS 설정 수정

--- src/main.ts
+++ src/main.ts
@@ -6,17 +6,24 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
+  const isDevelopment = process.env.NODE_ENV === 'development';
+
+  const host = process.env.HOST || '127.0.0.1';
+  const port = parseInt(process.env.PORT, 10) || 3000;
+
+  const corsOptions = {
+    origin: isDevelopment ? '*' : `${host}:${port}`,
+  };
+  app.enableCors(corsOptions);
 
   app.setGlobalPrefix('api');
 
   const config = new DocumentBuilder().setTitle('와씽').addBearerAuth().build();
-
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/docs', app, document);
 
   app.useGlobalFilters(new HttpExceptionFilter());
 
-  await app.listen(3000);
+  await app.listen(port);
 }
 bootstrap();
